a85c5e86381d68ad48b26a83787602635b6d94d8
illiyyin <iilliyyin@gmail.com>
2019-04-23 09:03:50 +0700

    second

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Product from './Product';
 
 class App extends Component {
--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Product.css';
+// import './Product.css';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,26 +7,102 @@ import './App.css';
 class Product extends React.Component {
   render() {
       return (
-          <div className="prdct">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="ungu"> Hello {this.props.nama} world from Component (class)</h1>
-            <div className="table">
-            <table >
-              <tbody>
-                <td><img src={logo} className="App-logo" alt="logo" />
-            <h1 className="ungu"> Harga HP</h1>
-            <h3>Rp 10.000</h3></td>
-                <td><img src={logo} className="App-logo" alt="logo" />
-            <h1 className="ungu"> Harga HP</h1><h3>Rp 10.000</h3></td>
-                <td><img src={logo} className="App-logo" alt="logo" />
-            <h1 className="ungu"> Harga HP</h1><h3>Rp 10.000</h3></td>
-                <td><img src={logo} className="App-logo" alt="logo" />
-            <h1 className="ungu"> Harga HP</h1><h3>Rp 10.000</h3></td>
+        <div className="row">
+
+        
+        <div class="col s6 m4 ">
+        <div class="col m12">
+        <div class="card  ">
+        <div class="card-content ">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+        <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons right">add</i>Tambah
+    
+  </button>
+          
+        </div>
+      </div>
+        </div>
+        </div>
+
+        <div class="col m8">
+    <div class="col s6 m4">
+    <div class="card">
+        <div class="card-image">
+        <img className="App-logo" src={logo}/>
+        <span className="card-title grey-text" >Card Title</span>
+                </div>
+        <div class="card-content">
+        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+        <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons right">delete</i>Hapus
+    
+    </button>
+        </div>
+    </div>
+    </div>
+    <div class="col s6 m4">
+    <div class="card">
+        <div class="card-image">
+        <img className="App-logo" src={logo}/>
+        <span className="card-title grey-text">Card Title</span>
+                </div>
+        <div class="card-content">
+        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+        <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons right">delete</i>Hapus
+    
+    </button>
+        </div>
+    </div>
+    </div>
+    <div class="col s6 m4">
+    <div class="card">
+        <div class="card-image">
+        <img className="App-logo" src={logo}/>
+        <span className="card-title grey-text">Card Title</span>
+        </div>
+        <div class="card-content">
+        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+        <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons right">delete</i>Hapus
+    
+    </button>
+        </div>
+    </div>
+    </div>
+    </div>
+
+            
+        </div>
+
+
+        //   <div className="prdct">
+        //     <img src={logo} className="App-logo" alt="logo" />
+        //     <h1 className="ungu"> Hello {this.props.nama} world from Component (class)</h1>
+        //     <div className="table">
+        //     <table >
+        //       <tbody>
+        //         <td><img src={logo} className="App-logo" alt="logo" />
+        //     <h1 className="ungu"> Harga HP</h1>
+        //     <h3>Rp 10.000</h3></td>
+        //         <td><img src={logo} className="App-logo" alt="logo" />
+        //     <h1 className="ungu"> Harga HP</h1><h3>Rp 10.000</h3></td>
+        //         <td><img src={logo} className="App-logo" alt="logo" />
+        //     <h1 className="ungu"> Harga HP</h1><h3>Rp 10.000</h3></td>
+        //         <td><img src={logo} className="App-logo" alt="logo" />
+        //     <h1 className="ungu"> Harga HP</h1><h3>Rp 10.000</h3></td>
                 
-              </tbody>
-            </table>
-          </div>
-          </div>
+        //       </tbody>
+        //     </table>
+        //</div>   </div>
+        //   </div>
           
       );
   }
